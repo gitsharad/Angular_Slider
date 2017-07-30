8460c2912876b0c8218d8bf8a6ba3a76ef81c69a
Shaggy <ssspawar25@gmail.com>
2017-07-30 17:37:22 +0530

    navigation heighlighted as per slide index on prev and next

--- js/app.js
+++ js/app.js
@@ -30,32 +30,30 @@ slider_module.directive("sliderDirective", function ($timeout) {
 
 	return {
 		controller: function ($scope, $element, $attrs) {
-			var slideindex = 0;
+			$scope.slideindex = 0;
 			angular.element($element[0].children[0]).css("display", "block");
 			$scope.onSlide = function (k) {
-                slideindex = slideindex + k;
-				if (slideindex < 0) {
-					slideindex = $element[0].children.length - 1;
-                } else if (slideindex >= $element[0].children.length) {
-					slideindex = 0;
+                $scope.slideindex = $scope.slideindex + k;
+				if ($scope.slideindex < 0) {
+					$scope.slideindex = $element[0].children.length - 1;
+                } else if ($scope.slideindex >= $element[0].children.length) {
+					$scope.slideindex = 0;
                 }
 				for (var i = 0; i < $element[0].children.length; i++) {
                      angular.element($element[0].children[i]).css("display", "none");
                 }
-                     angular.element($element[0].children[slideindex]).css("display", "block");
+                     angular.element($element[0].children[$scope.slideindex]).css("display", "block");
             }
 
 			//Code To Execute on Slide Nav Click
 			$scope.onSlideNav = function (el,j) {
-                     var slideindex=j;
-					 //console.log("i am in on slide nav click",angular.element(el).css('border','1px solid gray'));
-					 //angular.element(el);
-					 //angular.element(el).css("border","2px solid black");
-				for (var i = 0; i < $element[0].children.length; i++) {
+                    $scope.slideindex=j;
+					for (var i = 0; i < $element[0].children.length; i++) {
                      angular.element($element[0].children[i]).css("display", "none");
-                }
-                     angular.element($element[0].children[slideindex]).css("display", "block");
-            }
+					}
+                     angular.element($element[0].children[$scope.slideindex]).css("display", "block");
+					 
+            } 
 		}
 	}
 
